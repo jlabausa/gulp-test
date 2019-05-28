9735b27641d1ecc3b44ff9f39bd34e9a772ce528
j.labausa <j.labausa@creativehope.jp>
2019-05-28 15:41:22 +0900

    Setting up Gulp JS files, including node files and basic setup for gulpfile.js .

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+
+/** TOP LEVEL FUNCTIONS (C/o: Traversy Media)
+
+	1. gulp.task - Defines tasks
+	2. gulp.src - Points to files to use
+	3. gulp.dest - Points to folder to output
+	4. gulp.watch - Watch files and folders for changes
+
+--------------------------------------------------*/
